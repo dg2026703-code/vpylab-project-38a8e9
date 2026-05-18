1c8c3b1f1f9c3d0121d2efc78174d59548bb29d3
dg2026703-code <dg2026703@danggok.hs.kr>
2026-05-19 08:55:22 +0900

    처음 코드
    — 10703김예훈 via VPyLab

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from vpython import *
+sphere(pos=vector(0,0,0))
 
-# 구를 만들고 움직여보세요
-ball = sphere(pos=vector(0, 0, 0), radius=0.5, color=color.cyan)
-ball.velocity = vector(1, 0, 0)
+box(pos=vector(0,0,0.9),size=vector(17,10,0.5))
 
-dt = 0.01
-while True:
-    rate(100)
-    ball.pos = ball.pos + ball.velocity * dt
+box(pos=vector(0,0,1.2),size=vector(16,9,0.2),color=color.red)
 
-    # 벽에 부딪히면 반사
-    if abs(ball.pos.x) > 5:
-        ball.velocity.x = -ball.velocity.x
+box(pos=vector(0,-5,0),size=vector(1,10,1))
+
+box(pos=vector(0,-10,0),size=vector(5,0.1,5))
+
+box(pos=vector(8,-10,5),size=vector(3,1,5))
+
+box(pos=vector(8,-9.5,4),size=vector(0.5,0.1,3),color=color.black)
+
+b = box(make_trail = True)
+
+while True : 
+    rate(700)
+    k=keysdown()
+    if ' ' in k:
+        b.color = color.blue
+        
+        
+        
+        
+        
+        
+    
+
+
+
